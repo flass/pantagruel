d360384d8d3a287fffdd3f6db27b1273242fa532
Philippe Veber <philippe.veber@gmail.com>
2018-02-15 18:17:36 +0100

    ocamlification scripts

--- app/pantagruel_app.ml
+++ app/pantagruel_app.ml
@@ -5,8 +5,8 @@ open Bistro_utils
 let run ~outdir ~taxid () =
   let pipeline = Pipeline.make ~taxid in
   let repo = Pipeline.repo pipeline in
+  (* Lwt_main.run (Entrez.assembly_request ~taxid) ; *)
   Repo.build ~outdir repo
-
 
 let command =
   let open Command.Let_syntax in
@@ -17,7 +17,8 @@ let command =
         flag "--outdir" (required string) ~doc:"PATH Destination directory."
       and taxid =
         flag "--taxid"  (required int) ~doc:"INTEGER NCBI taxid" in
-      fun () -> run ~outdir ~taxid ()
+      fun () ->
+        run ~outdir ~taxid ()
     ]
 
 let () = Command.run ~version:"dev" command
