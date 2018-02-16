83d75517b9c239814a21937af01647e3194d7ea9
Philippe Veber <philippe.veber@gmail.com>
2018-02-16 17:33:10 +0100

    pipeline up to generation of families ok

--- app/pantagruel_app.ml
+++ app/pantagruel_app.ml
@@ -2,13 +2,13 @@ open Core
 open Pantagruel
 open Bistro_utils
 
-let run ~outdir ~taxid () =
-  let pipeline = Pipeline.make ~taxid in
+let run ~outdir ~assembly_folder () =
+  let pipeline = Pipeline.make assembly_folder in
   let repo = Pipeline.repo pipeline in
   (* Lwt_main.run (Entrez.assembly_request ~taxid) ; *)
   Repo.build ~outdir repo
 
-let command =
+let command1 =
   let open Command.Let_syntax in
   Command.basic
     ~summary:"Pantagruel"
@@ -18,7 +18,20 @@ let command =
       and taxid =
         flag "--taxid"  (required int) ~doc:"INTEGER NCBI taxid" in
       fun () ->
-        run ~outdir ~taxid ()
+        (* run ~outdir ~taxid *) ()
+    ]
+
+let command =
+  let open Command.Let_syntax in
+  Command.basic
+    ~summary:"Pantagruel"
+    [%map_open
+      let outdir =
+        flag "--outdir" (required string) ~doc:"PATH Destination directory."
+      and assembly_folder =
+        flag "--input"  (required string) ~doc:"PATH Assembly folder" in
+      fun () ->
+        run ~outdir ~assembly_folder ()
     ]
 
 let () = Command.run ~version:"dev" command
--- lib/pipeline.ml
+++ lib/pipeline.ml
@@ -1,10 +1,55 @@
 open Core
+open Bistro.Std
+open Bistro_bioinfo.Std
+open Bistro.EDSL
 open Bistro_utils
 
-let make ~taxid =
+let tools_env = docker_image ~account:"pveber" ~name:"pantagruel-tools" ~tag:"latest" ()
+
+let collect_all_proteins assembly_folder =
+  workflow ~descr:"collect_all_proteins" [
+    cmd "bash" ~env:tools_env [
+      file_dump (string Scripts.collect_all_proteins) ;
+      dep assembly_folder ;
+      dest ;
+    ] ;
+  ]
+
+let dereplicate_fasta (fa : fasta workflow) : fasta workflow =
+  workflow ~descr:"dereplicate_fasta" [
+    cmd "python" ~env:tools_env [
+      file_dump (string Scripts.dereplicate_fasta) ;
+      dep fa ;
+      dest ;
+    ] ;
+  ]
+
+let split_mmseqs_clustdb_fasta fa =
+  workflow ~descr:"split_mmseqs_clustdb_fasta" [
+    cmd "python" ~env:tools_env [
+      file_dump (string Scripts.split_mmseqs_clustdb_fasta) ;
+      dep fa ;
+      string "ABCDEP" ;
+      dest ;
+    ]
+  ]
+
+let make assembly_folder_path =
+  let assembly_folder = input assembly_folder_path in
+  let all_unique_proteins =
+    collect_all_proteins assembly_folder
+    |> dereplicate_fasta
+  in
+  let protein_families =
+    let open Mmseqs2 in
+    let db = createdb all_unique_proteins in
+    createseqfiledb db (cluster db) in
   object
+    method output = split_mmseqs_clustdb_fasta protein_families
   end
 
 
 let repo p =
-  Repo.[]
+  Repo.[
+    item ["output"] p#output ;
+  ]
