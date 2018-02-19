7f5b196a28ce2e0fe16a3e00804c111c03fb77d8
flass <florentlassalle@hotmail.fr>
2018-02-19 11:24:55 +0100

    add collection of assembly metadata: sequencing tech, assembly level contig count and N50

--- pantagruel_pipeline_sqlite.sh
+++ pantagruel_pipeline_sqlite.sh
@@ -79,6 +79,19 @@ patcontig="all\tall\tall\tall\tcontig-"
 # assembly status
 sedfilter0="s@\(.\+\)/.\+${filetag}:# ${patasslev}: \(.\+\)@\1\t\2@g"
 grep "$patasslev" */*${filetag} | sed -e "$sedfilter0" | sed -e 's/\r//g' > ${indata}/assembly_stats/assembly_level.tab
+# assembly status
+sedfilter0="s@\(.\+\)/.\+${filetag}:# ${patasslev}: \(.\+\)@\1\t\2@g"
+grep "$patasslev" */*${filetag} | sed -e "$sedfilter0" | sed -e 's/\r//g' > ${indata}/assembly_stats/assembly_level.tab
+# sequencing technology
+sedfilter1="s@\(.\+\)/.\+${filetag}:# ${patseqteq}: \(.\+\)@\1\t\2@g"
+sedfilter2="s@\(.\+\)/.\+${filetag}@\1\tNA@g"
+grep "$patseqteq" */*${filetag} | sed -e "$sedfilter1" | sed -e 's/\r//g' > ${indata}/assembly_stats/sequencing_technology.tab
+grep -L "$patseqteq" */*${filetag} | sed -e "$sedfilter2" | sed -e 's/\r//g' >> ${indata}/assembly_stats/sequencing_technology.tab
+# contigs
+for info in count N50 ; do
+  sedfilter3="s@\(.\+\)/\1${filetag}:${patcontig}${info}\t\(.\+\)@\1\t\2@g"
+  grep -P $patcontig */*${filetag} | sed -e $sedfilter3 | grep -v $filetag > ${indata}/assembly_stats/contig-${info}
+done
 
 mkdir ${indata}/complete_genomes/
 rm -f ${allcomplete}*
