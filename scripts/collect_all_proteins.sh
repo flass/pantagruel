c203049b728963487bbb4148e63ef3e7faf07b1b
Philippe Veber <philippe.veber@gmail.com>
2018-02-19 13:24:01 +0100

    scripts/collect_all_proteins: modified CLI

--- scripts/collect_all_proteins.sh
+++ scripts/collect_all_proteins.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-assembly_folder=$1
+assembly_list=$1
 DEST=$2
 
-assemblies=$(ls ${assembly_folder}/GC[AF]_* -d)
-faas=$(echo $assemblies | sed -e 's#\([^ ]\+\)/\([^/ ]\+\)#\1/\2/\2_protein\.faa\.gz#g')
+faas=$(cat assembly_list | sed -e 's#\([^ ]\+\)/\([^/ ]\+\)#\1/\2/\2_protein\.faa\.gz#g')
 for faa in $faas; do zcat $faa ; done > ${DEST}
