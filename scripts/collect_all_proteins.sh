16a55b6a168124d752b5ff0934bc038190e936de
Philippe Veber <philippe.veber@gmail.com>
2018-02-19 18:29:26 +0100

    scripts/collect_all_proteins.sh: fixed trivial bug

--- scripts/collect_all_proteins.sh
+++ scripts/collect_all_proteins.sh
@@ -3,5 +3,5 @@
 assembly_list=$1
 DEST=$2
 
-faas=$(cat assembly_list | sed -e 's#\([^ ]\+\)/\([^/ ]\+\)#\1/\2/\2_protein\.faa\.gz#g')
+faas=$(cat $assembly_list | sed -e 's#\([^ ]\+\)/\([^/ ]\+\)#\1/\2/\2_protein\.faa\.gz#g')
 for faa in $faas; do zcat $faa ; done > ${DEST}
