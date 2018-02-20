0ffd32a8b7fbf05aa9070c665b19b049d7235cef
flass <florentlassalle@hotmail.fr>
2018-02-20 12:36:31 +0000

    bugfix in populating SQLite db

--- pantagruel_pipeline.sh
+++ pantagruel_pipeline.sh
@@ -247,10 +247,10 @@ cd ${database}
 ${dbscripts}/pantagruel_sqlitedb.sh ${database} ${entdbname,,} ${allcomplete} ${protali} ${protfamseqs}.tab ${protorfanclust} ${cdsorfanclust} ${dbscripts}/pantagruel_sqlitedb_initiate.sql ${dbscripts}/pantagruel_sqlitedb_populate.py
 
 # generate reference for translation of genome assembly names into short identifier codes (uing UniProt "5-letter" code when available).
-sqlite3 -separator '\t' ${sqldbname} "select assembly_id, code from genome.assemblies;" > $database/genome_codes.tab
+sqlite3 ${sqldbname} "select assembly_id, code from genome.assemblies;" | sed -e 's/|/\t/g' > $database/genome_codes.tab
 # and CDS names into code of type "SPECIES_CDSTAG" (ALE requires such a naming)
-# here split the lines with '|' and remove 1st field that is always '|'
-sqlite3 -separator "\t" ${sqldbname} "select genbank_cds_id, cds_code from coding_sequences;" | awk -F'|' '{print $NF}' | sed -e 's/|/\t/g' > $database/cds_codes.tab
+# here split the lines with '|' and remove GenBank CDS prefix that is always 'lcl|'
+sqlite3 ${sqldbname} "select genbank_cds_id, cds_code from coding_sequences;" | sed -e 's/lcl|//g'  | sed -e 's/|/\t/g' > $database/cds_codes.tab
 
 # translates the header of the alignment files
 export alifastacodedir=${protali}/full_cdsfam_alignments_species_code
