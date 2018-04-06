c94b622b993169f88c98c9f41f410546ed194632
Florent Lassalle <florentlassalle@hotmail.fr>
2018-04-06 11:43:25 +0100

    debug scripts/genefam_table_as_list.py

--- scripts/genefam_table_as_list.py
+++ scripts/genefam_table_as_list.py
@@ -6,11 +6,15 @@ nffam = sys.argv[1]
 nfout = sys.argv[2]
 writefamname = bool(sys.argv[3])
 
+# could just read input file and write as it reads family blocks
+# however non-efficient use of a dict to store family is prefered
+# to deal with generic case where families are not sorted in imput file
 dfam = {}
 
 with open(nffam, 'r') as ffam:
-	fam, prot = line.rstrip('\n').split('\t')
-	dfam.setdefault(fam, []).append(prot)
+	for line in ffam:
+		fam, prot = line.rstrip('\n').split('\t')
+		dfam.setdefault(fam, []).append(prot)
 
 with open(nfout, 'w') as fout:
 	for fam, prots in dfam.iteritems():
