a756858056fb33eb498ee9f30e58c82e2e9e074b
Florent Lassalle <florentlassalle@hotmail.fr>
2018-04-30 17:38:46 +0100

    add option of species name separator in scripts/restrict_aln_fasta.py

--- scripts/restrict_aln_fasta.py
+++ scripts/restrict_aln_fasta.py
@@ -4,6 +4,11 @@ from os import path
 nfinaln = sys.argv[1]
 nfoutaln = sys.argv[2]
 nfrestrictspelist = sys.argv[3]
+if len(sys.argv)>4:
+	sep=sys.argv[4]
+else:
+	sep=None
+
 with open(nfrestrictspelist, 'r') as frestrictspelist:
 	lrestrictspe = [line.rstrip('\n') for line in frestrictspelist]
 
@@ -13,7 +18,7 @@ with open(nfinaln, 'r') as fin:
   for line in fin:
    if line.startswith('>'):
     lsp = line.strip('>\n').split()
-    spe = lsp[0]
+    spe = lsp[0].split(sep)[0]
     if spe in lrestrictspe:
      w = True
     else:
