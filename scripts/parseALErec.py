77755ceeb94eaa9ef589641116d73ebd5d4ba410
Florent Lassalle <florentlassalle@hotmail.fr>
2018-07-13 12:34:26 +0200

    added prototype functions for obtaining orthologous groups from reconciled gene trees

--- scripts/parseALErec.py
+++ scripts/parseALErec.py
@@ -141,7 +141,7 @@ def parseUndatedRecGeneTree(recgt, spet, dexactevt={}, recgtsample=None, nsample
 					#~ # a simple speciation event ; already delt with
 					#~ pass
 		if excludeTaggedSubtrees:
-			alllabext = [lab.split('_', 1)[1] for lab in node.get_leaf_labels()]
+			alllabext = [lab.split('_', 1)[1] for lab in node.iter_leaf_labels()]
 			if (len(set(alllabext)) == 1) and (excludeTaggedSubtrees in alllabext[0]):
 				# the subtree below this node is an artificial addition to the reconciled gene tree and should be skipped
 				return
@@ -161,22 +161,213 @@ def parseRecGeneTree(recgt, spet, ALEmodel='undated', **kw):
 	elif ALEmodel=='dated':
 		return parseDatedRecGeneTree(recgt, spet, **kw)
 
-def getOrthologues(recgt, spet, ALEmodel='dated'):
-	"""prune reconciled gene tree at transfer or duplication branches to identify subs of orthologues"""
-	for node in recgt.get_postordertraversal_children():
-		nodelab = node.label()
-		if node.is_leaf() and (excludeTaggedLeaves in nodelab):
-			# the species assignment of this leaf is not certain (inferred)
-			# and thus events leading directly to this leaf are not to be trusted and should be skipped
-			return
-		nodeid = node.nodeid()
-		if not nodelab:
-			print node
-			raise ValueError, "unannotated node"
-		# line of events to be read left-to-right backward in time
-		lineage = nodelab.split('.')
-		for i in range(1, len(lineage)):
-			eventlab = lineage[i]
+#### dated model gene tree syntax parsing
+def findNextDatedEventSlice(nodelab, receptionOK=False):
+	nextev1 = nodelab.find('@')
+	if nextev1 > 0:
+		if nodelab[nextev1-1] in {'T', 'D'}:
+			nextev1 -=1		# account for the preceding D or T
+		elif not receptionOK:
+			raise IndexError
+	nextev2 = nodelab.find('.')
+	if (nextev1 < 0) and (nextev2 < 0):
+		return None
+	elif (nextev1 >= 0) and ((nextev2 < 0) or (nextev1 < nextev2)):
+		return nextev1
+	elif (nextev2 >= 0) and ((nextev1 < 0) or (nextev2 < nextev1)):
+		return nextev2
+	else:
+		raise IndexError
+
+def splitSingleUndatedEvent(nodelab, isleaf=False):
+	"""yet to implement"""
+	raise NotImplementedError
+	lineage = nodelab.split('.') # bring in code from parseUndatedRecGeneTree()
+	return lineage
+
+def getLeafLabelDatedEvent(nodelab):
+	# remove leaf label prefix before parsing (possible) trailing event chain
+	nextev = findNextDatedEventSlice(nodelab, receptionOK=True)
+	leaflab = nodelab[:nextev] # the whole string if nextev is None
+	return leaflab
+
+def splitSingleDatedEvent(nodelab, isleaf=False):
+	"""from a gene tree branch event chain, yield the type and location of every event, and if a leaf, finishes by the leaf label"""
+	if isleaf:
+		# remove leaf label prefix before parsing (possible) trailing event chain
+		nextev = findNextDatedEventSlice(nodelab, receptionOK=True)
+		leaflab = nodelab[:nextev] # the whole string if nextev is None
+		# fist yield
+		yield leaflab
+		s = nodelab[len(leaflab):]
+	else:
+		s = nodelab
+	i = 0
+	while s:
+		print s
+		reception = False
+		# process separators in order
+		if s.startswith('.T@'):
+			# transfer-loss // emission of transfer by donor followed by loss of resident copy; just changes species identity of the gene tree branch (from donor to recipent species)
+			# will be followed by transfer reception event
+			reception = True
+			thisev = 3
+			evtype = 'TdL'
+		elif s.startswith('.'):
+			if i==0:
+				# simple speciation
+				evtype = 'S'
+			else:
+				# speciation-loss // speciation followed by loss in the sister lineage; just changes species identity of the gene tree branch (from parent to daughter species)
+				evtype = 'SL'
+			thisev = 1
+		elif s.startswith('@'):
+			# reception of transfer by recipient = gain
+			thisev = 1
+			evtype = 'Tr'
+		elif s.startswith('T@'):
+			# emission of transfer by donor; not relevant to gene content
+			thisev = 2
+			evtype = 'Td'
+		elif s.startswith('D@'):
+			# duplication = gain
+			thisev = 2
+			evtype = 'D'
+		elif isleaf:
+			# final speciation
+			evtype = 'S'
+			thisev = 0
+		else:
+			raise ValueError
+		i += 1
+		nextev = findNextDatedEventSlice(s[thisev:], receptionOK=reception)
+		if nextev is None:
+			# no other event
+			yield (evtype, s[thisev:])
+			s = None
+		else:
+			yield (evtype, s[thisev:nextev+thisev])
+			s = s[nextev+thisev:]
+
+def splitEventChain(nodelab, isleaf=False, ALEmodel='dated'):
+	if ALEmodel=='undated':
+		lineage = [event for event in splitSingleDatedEvent(nodelab, isleaf=isleaf)]
+	elif ALEmodel=='dated':
+		lineage = [event for event in splitSingleDatedEvent(nodelab, isleaf=isleaf)]
+		if isleaf:
+			# remove the first-yielded leaf label from vent list
+			leaflab = lineage[0]
+			lineage = lineage[1:]
+		else:
+			leaflab = None
+		# line of events is read left-to-right FORWARD in time; reverse it for a BACKWARD result
+		lineage = reversed(lineage)
+	return (lineage, leaflab)
+
+def _prune_orthologs_bottom_up(node, ALEmodel='dated', **kw):
+	"""strict definition of ortholous groups: only those related by a line of speciation events (no transfer, no duplication) are orthologs"""
+	# initiate at root
+	unclassified = set([])
+	orthologGroups = kw.get('orthologGroups', [])
+	dlabs = kw.get('dlabs', {})
+	nodelab = node.label()
+	if not nodelab: raise ValueError, "unannotated node:\n%s"%str(node)
+	lineage, leaflab = splitEventChain(nodelab, isleaf=node.is_leaf(), ALEmodel=ALEmodel)
+	if node.is_leaf():
+		unclassified.add(leaflab) # where unclasified set is filled up
+		dlabs[nodelab] = leaflab
+	else:
+		# post-order traversal recursion:
+		for child in node.children:
+			# first explore children
+			orthologGroups, uncl, dlabs = _prune_orthologs_bottom_up(child, orthologGroups=orthologGroups, dlabs=dlabs, ALEmodel=ALEmodel)
+			unclassified |= uncl
+	# then explore node itself
+	if not unclassified:
+		# all leaves below have been already classified in orthologous group; stop here.
+		return orthologGroups, unclassified, dlabs, orthoSubs
+	else:
+		# list of events must go BACKWARD in time when read left-to-right
+		for event in lineage:
+			evtype, evloc = event
+			if evtype in {'Tr', 'T'}:
+				# gene was last gained here by a species ancestor:
+				# what was not yet classifed in this subtree is an orthologous group
+				ortho = tuple(sorted(unclassified))
+				orthologGroups.append(ortho)
+				unclassified = set([])
+				break
+			elif evtype=='D':
+				# gene was duplicated here by species ancestor:
+				# what was not yet classifed in the child subtrees
+				# of this subtree are each an orthologous group
+				for child in node:
+					subuncl = set([dlabs[leaflabevchain] for leaflabevchain in child.iter_leaf_labels()]) & unclassified
+					ortho = tuple(sorted(subuncl))
+					orthologGroups.append(ortho)
+				unclassified = set([])
+				break
+		return orthologGroups, unclassified, dlabs
+
+def _prune_orthologs_top_down(node, refspetree=None, ALEmodel='dated', **kw):
+	assert refspetree
+	# initiate at root
+	orthologGroups = kw.get('orthologGroups', [])
+	dlabs = kw.get('dlabs', {})
+	splitparam0, splitparam1 = kw.get('splitparam', ('_',1))
+	# first establish the dictionary of actual leaf lables, without the trailing event chain
+	for nodelab in node.iter_leaf_labels():
+		dlabs[nodelab] = getLeafLabelDatedEvent(nodelab)
+	if not nodelab: raise ValueError, "unannotated node:\n%s"%str(node)
+	lineage, leaflab = splitEventChain(nodelab, isleaf=node.is_leaf(), ALEmodel=ALEmodel)
+	# list of events must go BACKWARD in time when read left-to-right
+	for event in lineage:
+		evtype, evloc = event
+		if evtype in {'Tr', 'T'}:
+			leaflabs = [dlabs[leaflabevchain] for leaflabevchain in node.iter_leaf_labels()]
+			lspe = [leaflab.split(splitparam0, splitparam1)[0] for leaflab in leaflabs]
+			sspe = set(lspe)
+			if len(lspe) < len(sspe):
+				# not unicopy
+				break # for event loop
+			else:
+				speanc = refspetree[evloc]
+				assert isinstance(speanc, tree2.Node)
+				clademrca = refspetree.mrca(sspe)
+				if clademrca.is_childorself(speanc):
+					ortho = tuple(sorted(leaflabs))
+					orthologGroups.append(ortho)
+					break # for event loop
+	else:
+		# if did not find the clade to be an orthologous group, recurse down the tree:
+		for child in node.children:
+			orthologGroups, dlabs = _prune_orthologs_top_down(node, orthologGroups=orthologGroups, dlabs=dlabs, refspetree=refspetree, ALEmodel=ALEmodel)
+	return orthologGroups, dlabs
+
+def getOrthologues(recgt, ALEmodel='dated', method='strict', **kw):
+	"""prune reconciled gene tree at transfer or duplication branches to identify subs of orthologues. 
+	
+	several methods are possible:
+	'strict':
+	    bottom-up approach (tip-to-root) where only those leaves related by a line of speciation events (no transfer, no duplication) are orthologs; 
+	    the search is achieved by 
+	'unicopy_clades':
+	    top-down approach (root-to-tips) where orthologous groups are defined as the gene tree clades containing only unique representatives of species  
+	    that belong to the species clade below the ancestor to which a gene gain is mapped (last recipient of the gene if several gain events occurred 
+	    on the gene tree branch). This allows to recognize as orthologous groups  clades where transfer events within the clade that do not induce 
+	    change of copy number (when considering the sum of all events in the subtree), i.e. gene conversion events
+	'mixed': (heuristic ?) approach mixing the two previous: pruning of transfers in the bottom-up approach may reveal bona-fide unicopy clades of orthologs.
+	
+	using the last two options requires to provide a reference species tree, passed on with the keyword argument 'refspetree'.
+	"""
+	if method=='strict':
+		return _prune_orthologs_bottom_up(recgt, ALEmodel=ALEmodel, **kw)
+	elif method=='unicopy_clades':
+		refspetree = kw['refspetree']
+		return _prune_orthologs_top_down(recgt, refspetree=None, ALEmodel=ALEmodel, **kw)
+	
+
+
 		
 	
 	
