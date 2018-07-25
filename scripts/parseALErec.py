766cc0811b370252790ae3fe3995fc4c4b2aa2e5
Florent Lassalle <florentlassalle@hotmail.fr>
2018-07-25 11:20:35 +0100

    debug

--- scripts/parseALErec.py
+++ scripts/parseALErec.py
@@ -385,7 +385,7 @@ def _prune_nested_candidate_orthologs(node, dsbal, leaflabs, lspe, extraspe, can
 				# second keep only those that intersect with the set of possible leaves to remove
 				cOGs.append(scOG)
 		# sort first the cOG with the highest score of leaf set overlap, then smallest size, then higher branching point in gene tree
-		cOGs.sort(key=lambda x: -(len(sextraleaflabs & x), len(x), branching_depth(x))) 
+		cOGs.sort(key=lambda x: (-len(sextraleaflabs & x), len(x), branching_depth(x))) 
 		return cOGs, sextraleaflabs
 	
 	sp0, sp1 = kw.get('splitparam', ('_',1))
