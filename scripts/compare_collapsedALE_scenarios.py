5c63d23c307aabbb612385cc621e123ee3f5bf18
Florent Lassalle <florentlassalle@hotmail.fr>
2018-08-08 13:13:42 +0100

    debug when single event type

--- scripts/compare_collapsedALE_scenarios.py
+++ scripts/compare_collapsedALE_scenarios.py
@@ -43,7 +43,7 @@ def _select_lineage_event_clause_factory(evtypes, valtoken, lineagetable):
 	if evtypes:
 		evtyperestrictIJ = "INNER JOIN species_tree_events USING (event_id)"
 		if len(evtypes)>1: evtyperestrictWC = "AND event_type IN %s"%repr(tuple(e for e in evtypes))
-		else: evtyperestrictWC = "AND event_type='%s'"
+		else: evtyperestrictWC = "AND event_type='%s'"%evtypes
 	else:
 		evtyperestrictWC = evtyperestrict = ""
 	return (rlocdsIJ, evtyperestrictIJ, evtyperestrictWC)
