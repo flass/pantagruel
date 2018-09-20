e9eca2e88db8300dec11a7bb2cb4f92fd955279a
Florent Lassalle <florentlassalle@hotmail.fr>
2018-09-20 18:05:19 +0100

    line-counting utility script

---
+++ scripts/count_lines_matching_files.py
@@ -0,0 +1,24 @@
+import sys, glob
+from os import path
+
+target = sys.argv[1]
+
+def file_len(fname):
+	with open(fname) as f:
+		for i, l in enumerate(f):
+			pass
+	return i + 1
+
+lnf = glob.glob(target)
+
+lnumlines = []
+
+for nf in lnf:
+	with open(nf, 'r') as f:
+		lnumlines.append((path.basename(nf), file_len(nf)))
+
+with open(target.replace('*', '_')+'line_counts', 'w') as fout:
+	fout.write('\n'.join(['\t'.joint(t) for t in lnumlines]))
+
+
+		
