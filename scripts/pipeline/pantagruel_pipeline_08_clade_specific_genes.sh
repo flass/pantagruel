eeaeca4c449c50e90e1c216655289a1a28991e28
Florent Lassalle <florentlassalle@hotmail.fr>
2018-12-06 13:00:18 +0000

    update with new subprogram options and addition of pangenome_hclust.r

--- scripts/pipeline/pantagruel_pipeline_08_clade_specific_genes.sh
+++ scripts/pipeline/pantagruel_pipeline_08_clade_specific_genes.sh
@@ -34,15 +34,21 @@ cd ${ptgrepo} ; export ptgversion=$(git log | grep commit | cut -d' ' -f2) ; cd 
 ## and to transpose orthologus group classification of collapsed clade to member genes)
 
 if [ -z ${getOrpthologuesOptions} ] ; then
-  getOrpthologuesOptions=" --ale.model='dated' --methods='mixed' --max.frac.extra.spe=0.5 --majrule.combine=0.5 --colour.combined.tree"
+  getOrpthologuesOptions=" --ale.model=${rectype} --methods='mixed' --max.frac.extra.spe=0.5 --majrule.combine=0.5 --colour.combined.tree --use.unreconciled.gene.trees ${mbgenetrees} --unreconciled.format 'nexus' --unreconciled.ext '.con.tre'"
 fi
 if [ -z ${orthoColId} ] ; then
   orthocolid=1
 fi
+# derived parameters
+if [ ${ALEalgo} == 'ALEml_undated' ] ; then
+  export rectype='undat'
+else
+  export rectype='dated'
+fi
 # generate Ortholog Collection
 orthocol=ortholog_collection_${orthoColId}
 mkdir -p ${orthogenes}/${orthocol}
-${ptgscripts}/get_orthologues_from_ALE_recs.py -i ${outrecdir} -o ${orthogenes}/${orthocol} ${getOrpthologuesOptions} -T 8 &> $ptglogs/get_orthologues_from_ALE_recs_${orthocol}.log
+${ptgscripts}/get_orthologues_from_ALE_recs.py -i ${outrecdir} -o ${orthogenes}/${orthocol} ${getOrpthologuesOptions} &> $ptglogs/get_orthologues_from_ALE_recs_${orthocol}.log
 
 # import ortholog classification into database
 sqlite3 ${sqldb} """INSERT INTO ortholog_collections (ortholog_col_id, ortholog_col_name, reconciliation_id, software, version, algorithm, ortholog_col_date, notes) VALUES 
@@ -51,6 +57,7 @@ sqlite3 ${sqldb} """INSERT INTO ortholog_collections (ortholog_col_id, ortholog_
 ;
 """
 python ${ptgscripts}/pantagruel_sqlitedb_load_orthologous_groups.py ${sqldb} ${orthogenes}/${orthocol} "mixed" "majrule_combined_0.500000" ${orthocolid}
+python ${ptgscripts}/pantagruel_sqlitedb_load_orthologous_groups.py ${sqldb} ${orthogenes}/${orthocol} "unreconciled" "unicopy" ${orthocolid} 1
 
 ## extract sister clade pairs from the reference species tree for later clade-specific gene search
 python << EOF
@@ -81,6 +88,9 @@ echo $orthocol
 orthomatrad=${orthogenes}/${orthocol}/mixed_majrule_combined_0.5.orthologs
 python ${ptgscripts}/get_ortholog_presenceabsence_matrix_from_sqlitedb.py ${sqldb} ${orthomatrad} ${orthocolid}
 ${ptgscripts}/get_clade_specific_genes.r ${orthomatrad}_genome_counts.no-singletons.mat ${sqldb} ${orthocolid} ${speciestree} ${orthomatrad}
+
+# create clsutering based on the abs/pres matrix (using Jaccard Distance)
+${dbscripts}/pangenome_hclust.r ${orthomatrad} & 
 
 # list clade-specific orthologs
 export orthomatrad=${orthogenes}/${orthocol}/mixed_majrule_combined_0.5.orthologs
