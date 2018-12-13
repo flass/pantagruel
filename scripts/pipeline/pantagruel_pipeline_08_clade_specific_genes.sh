dd6ba926cf88ebbe1a17e68b20202898c963c026
Florent Lassalle <florentlassalle@hotmail.fr>
2018-12-13 12:34:56 +0000

    relocate GO enrichment tests to clade-specific gene search task

--- scripts/pipeline/pantagruel_pipeline_08_clade_specific_genes.sh
+++ scripts/pipeline/pantagruel_pipeline_08_clade_specific_genes.sh
@@ -60,12 +60,13 @@ python ${ptgscripts}/pantagruel_sqlitedb_load_orthologous_groups.py ${sqldb} ${o
 python ${ptgscripts}/pantagruel_sqlitedb_load_orthologous_groups.py ${sqldb} ${orthogenes}/${orthocol} "unreconciled" "unicopy" ${orthocolid} 1
 
 ## extract sister clade pairs from the reference species tree for later clade-specific gene search
+export cladedefs="${speciestree}_clade_defs"
 python << EOF
 import tree2
 reftree = tree2.Node(file='${speciestree}')
-nfout = "${speciestree}_clade_defs"
+nfout = '${cladedefs}'
 fout = open(nfout, 'w')
-fout.write('\t'.join(['', 'clade', 'sisterclade'])+'\n')
+fout.write('\t'.join(['', 'clade', 'sisterclade', 'name', 'maxabsin', 'maxpresout'])+'\n')
 k = 0
 for node in reftree:
   if len(node.children) != 2: continue
@@ -77,14 +78,19 @@ for node in reftree:
       k += 1
     focchildleaflabset = ','.join(sorted(child.get_leaf_labels()))
     sischildleaflabset = ','.join(sorted(child.go_brother().get_leaf_labels()))
-    fout.write('\t'.join([focchildlab, focchildleaflabset, sischildleaflabset])+'\n')
+    fout.write('\t'.join([focchildlab, '""', focchildleaflabset, sischildleaflabset, '0', '0'])+'\n')
 
 fout.close()
 EOF
+# the resulting fle ${cladedefs} can have information filled/changed in the following collumns:
+# - 'name':       a taxon or practical name for the clade
+# - 'maxabsin':   the maximum count of genomes in the focal clade missing the gene (default 0)
+# - 'maxpresout': the maximum count of genomes in the sister clade featuring the gene (default 0)
+# additionally, extra rows can be added with custom genome group definitions (they don't need to be clades)
 
 # generate abs/pres matrix
 orthocol=ortholog_collection_${orthocolid}
-echo $orthocol
+echo ${orthocol}
 orthomatrad=${orthogenes}/${orthocol}/mixed_majrule_combined_0.5.orthologs
 python ${ptgscripts}/get_ortholog_presenceabsence_matrix_from_sqlitedb.py ${sqldb} ${orthomatrad} ${orthocolid}
 
@@ -93,3 +99,73 @@ ${dbscripts}/pangenome_hclust.r ${orthomatrad} 1000 &
 
 # list clade-specific orthologs
 ${ptgscripts}/get_clade_specific_genes.r ${orthomatrad}_genome_counts.no-singletons.mat ${sqldb} ${orthocolid} ${speciestree} ${orthomatrad}
+
+## test GO term enrichment in gene sets
+
+## generate background term distribution for clades i.e. list all genes in their pangenome and associated terms
+export goterms=${funcannot}/GeneOntology
+mkdir -p ${goterms}
+# for the whole dataset
+sqlite3 -cmd ".mode tab" ${sqldb} "select distinct locus_tag, go_id from coding_sequences 
+left join functional_annotations using (nr_protein_id) 
+left join interpro2GO using (interpro_id) ;" > ${goterms}/${ngenomes}-genomes_pangenome_terms.tab
+# for all clades of the species tree
+export claderefgodir=${goterms}/clade_go_term_reference_sets
+mkdir -p ${claderefgodir}/
+tail -n +2 ${cladedefs} | while read cla cladedef siscladedef ; do
+  claspeset="'$(echo $cladedef | sed -e "s/,/','/g")'"
+  echo $cla $claspeset
+  cladest=${claderefgodir}/${cla}_pangenome_terms.tab
+  q="select distinct locus_tag, go_id from coding_sequences 
+  inner join replicons using (genomic_accession)
+  inner join assemblies using (assembly_id)
+  left join functional_annotations using (nr_protein_id) 
+  left join interpro2GO using (interpro_id) 
+  where code in (${claspeset})"
+  sqlite3 -cmd ".mode tab" ${sqldb} "${q};" > ${cladest}
+  sqlite3 -cmd ".mode tab" ${sqldb} "${q} and go_id not null;" > ${cladest}_nonull
+  ls -lh ${cladest}
+done
+
+export dirgotablescladespe=${orthomatrad}_specific_genes.tables_byclade_goterms_pathways
+export dirgoenrichcladespe=${goterms}/clade_go_term_enriched_cladespecific_vs_coregenome
+mkdir -p ${dirgoenrichcladespe}/
+# compare each clade-specific core genome (single repr sequence per OG) to its respective core genome (single repr sequence per OG)
+tail -n +2 ${cladedefs} | while read cla name cladedef siscladedef maxabs maxpres ; do
+  echo $cla
+  cladspego=${dirgotablescladespe}/mixed_majrule_combined_0.5.orthologs_specific_genes_${cla}_reprseq_goterms.tab
+  cut -f5,6 ${cladspego} | grep -v "NA$" > ${cladspego}_nonull
+  ${dbscripts}/clade_specific_genes_GOterm_enrichment_test.r \
+  --study_annots ${cladspego}_nonull  \
+  --population_annots ${claderefgodir}/${cla}_coregenome_terms.tab_nonull \
+  --out ${dirgoenrichcladespe}/${cla}_go_term_enriched_cladespecific_vs_coregenome.tab \
+  --algo "weight01" --stat "Fisher" &> ${raplogs}/clade_specific_vs_coregenome_${cla}_GOterm_enrichment_test.log
+  ls -lh ${dirgoenrichcladespe}/*_${cla}_* ; echo ""
+done &> ${raplogs}/cladespecific_vs_coregenome_genes_GOterm_enrichment_test.log
+
+export dirgotablescladespe=${orthomatrad}_specific_genes.tables_byclade_goterms_pathways
+export dirgoenrichcladespe=${goterms}/clade_go_term_enriched_cladespecific_vs_pangenome
+mkdir -p ${dirgoenrichcladespe}/
+# compare each clade-specific core genome (all sequences) to its respective pangenome (all sequences)
+tail -n +2 ${cladedefs} | while read cla name cladedef siscladedef maxabs maxpres ; do
+  echo $cla
+  cladspego=${dirgotablescladespe}/mixed_majrule_combined_0.5.orthologs_specific_genes_${cla}_allseq_goterms.tab
+  cut -f5,6 ${cladspego} | grep -v "NA$" > ${cladspego}_nonull
+  ${dbscripts}/clade_specific_genes_GOterm_enrichment_test.r \
+  --study_annots ${cladspego}_nonull  \
+  --population_annots ${claderefgodir}/${cla}_pangenome_terms.tab_nonull \
+  --out ${dirgoenrichcladespe}/${cla}_go_term_enriched_cladespecific_vs_pangenome.tab \
+  --algo "weight01" --stat "Fisher" &> ${raplogs}/clade_specific_vs_coregenome_${cla}_GOterm_enrichment_test.log
+  ls -lh ${dirgoenrichcladespe}/*${cla}* ; echo ""
+done &> ${raplogs}/cladespecific_vs_pangenome_genes_GOterm_enrichment_test.log
+
+# concatenate summary reports
+tail -n +2 ${cladedefs} | while read cla name cladedef siscladedef maxabs maxpres ; do
+  cla=clade${n}
+  echo "# ${cla} ${name}"
+  echo "# cladespe vs. core"
+  cat ${goterms}/41NeoPseudo_clade_go_term_enriched_cladespecific_vs_coregenome/41NeoPseudo_${cla}_go_term_enriched_cladespecific_vs_coregenome.tab
+  echo "# cladespe vs. pan"
+  cat ${goterms}/41NeoPseudo_clade_go_term_enriched_cladespecific_vs_pangenome/41NeoPseudo_${cla}_go_term_enriched_cladespecific_vs_pangenome.tab
+  echo "# - - - "
+done > ${goterms}/41NeoPseudo_clade_go_term_enriched_cladespecific_summary.tab
--- scripts/pipeline/pantagruel_pipeline_09_functional_annotation.sh
+++ scripts/pipeline/pantagruel_pipeline_09_functional_annotation.sh
@@ -19,11 +19,11 @@ source $envsourcescript
 cd ${ptgrepo} ; export ptgversion=$(git log | grep commit | cut -d' ' -f2) ; cd -
 
 ############################
-## 09. Functional annotation
+## 04. Functional annotation
 ############################
 
-export funcannot=${ptgdb}/09.functional
-mkdir -p ${funcannot}
+export funcannot=${ptgdb}/04.functional
+mkdir -p ${funcannot}/
 export IPversion=$(interproscan --version | head -n 1 | sed -e 's/InterProScan version //')
 if [ -z "$IPversion" ] ; then
   echo "Error unable to dertermine version of Interproscan; please verify the program is correctly installed ; exiting now."
@@ -65,55 +65,6 @@ done &
 
 
 ## load the annotation data into the database
-python ${ptgscripts}/pantagruel_sqlitedb_load_interproscan_annotations.py ${sqldb} ${interpro} ${IPversion}
-#~ python ${ptgscripts}/pantagruel_load_interproscan_annotations.py --sqlite_db ${sqldb} --ipscan_annot_files "${interpro}/all_complete_proteomes/*.tsv" --ipscan_version ${IPversion}
-
-
-sqlite3 -cmd ".mode tab" ${sqldb} "select distinct locus_tag, interpro_id, interpro_description, go_terms, pathways 
-from coding_sequences 
-inner join functional_annotations using (nr_protein_id) 
-inner join interpro_terms using (interpro_id) 
-where interpro_description like '%arsen%' order by locus_tag, interpro_id;" > ${funcannot}/arsen_interpro_matches_go_terms_pathways.tab
-
-## generate background term distribution for clades i.e. list all genes in their pangenome and associated terms
-export goterms=${funcannot}/GeneOntology
-mkdir -p ${goterms}
-# for the whole dataset
-sqlite3 -cmd ".mode tab" ${sqldb} "select distinct locus_tag, go_id from coding_sequences 
-left join functional_annotations using (nr_protein_id) 
-left join interpro2GO using (interpro_id) ;" > ${goterms}/${ngenomes}-genomes_pangenome_terms.tab
-# for clades of the $focus clade 41NeoPseudo
-export claderefgodir=${goterms}/${focus}_clade_go_term_reference_sets
-mkdir -p ${claderefgodir}/
-tail -n +2 ${corefocus}/${focus}_clade_defs | while read cla cladedef siscladedef ; do
-  claspeset="'$(echo $cladedef | sed -e "s/,/','/g")'"
-  echo $cla $claspeset
-  cladest=${claderefgodir}/${focus}_${cla}_pangenome_terms.tab
-  q="select distinct locus_tag, go_id from coding_sequences 
-  inner join replicons using (genomic_accession)
-  inner join assemblies using (assembly_id)
-  left join functional_annotations using (nr_protein_id) 
-  left join interpro2GO using (interpro_id) 
-  where code in (${claspeset})"
-  sqlite3 -cmd ".mode tab" ${sqldb} "${q};" > ${cladest}
-  sqlite3 -cmd ".mode tab" ${sqldb} "${q} and go_id not null;" > ${cladest}_nonull
-  ls -lh ${cladest}
-done
-
-## test GO term enrichment in gene sets
-catrefile (){
-  for cla in $1 ; do 
-    ls ${claderefgodir}/${focus}_${cla}_pangenome_terms.tab_nonull
-  done | xargs | tr ' ' ','
-}
-# first test general enrichment of a clade pangenome against the larger pangenome
-${ptgscripts}/clade_specific_genes_GOterm_enrichment_test.r \
---study_annots "$( catrefile 'clade8 clade9 clade10')" \
---population_annots "$( catrefile 'clade4')" \
---out ${goterms}/${focus}_clade_go_term_enriched_vs_clade4_pangenome \
---algo "weight01" --stat "Fisher" &> ${raplogs}/clade_specific_genes_GOterm_enrichment_test.log &
-#~ [1] 21044
-
-# then refresh clade-specific gene tables, adding GO term and pathway annotations
-${ptgscripts}/get_clade_specific_genes.r ${orthomatrad}_genome_counts.no-singletons.mat ${sqldb} ${orthocolid} ${coregenome}/${focus}/${focus} ${orthomatrad}
+#~ python ${ptgscripts}/pantagruel_sqlitedb_load_interproscan_annotations.py ${sqldb} ${interpro} ${IPversion}
+python ${ptgscripts}/pantagruel_load_interproscan_annotations.py --sqlite_db ${sqldb} --ipscan_annot_files "${interpro}/all_complete_proteomes/*.tsv" --ipscan_version ${IPversion}
 
