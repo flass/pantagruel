f390044a734a82ecf1a9120d0486a93757ad1b31
Florent Lassalle <florentlassalle@hotmail.fr>
2019-01-18 13:23:08 +0000

    debug install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -53,8 +53,8 @@ fi
 
 # install R packages not supported by Debian
 R --vanilla <<EOF
-install.packages('topGO', repos=${CRANmirror})
-install.packages('pvclust', repos=${CRANmirror})
+install.packages('topGO', repos='${CRANmirror}')
+install.packages('pvclust', repos='${CRANmirror}')
 EOF
 echo "library('topGO')" | R --vanilla
 checkexec "Could not install R package 'topGO'"
