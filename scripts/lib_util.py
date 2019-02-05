e8c24af1d5d980865ad29ef84bf47cd69e2ea3d8
Florent Lassalle <florentlassalle@hotmail.fr>
2019-02-05 12:25:10 +0000

    fix CDS/prot sequence name parsing

--- scripts/lib_util.py
+++ scripts/lib_util.py
@@ -77,11 +77,6 @@ def concatAln(aln_file_list, filename, format="interleaved"):
 	for aln_file in aln_file_list:
 		#print aln_file
 		aln=AlnGenerator(aln_file)
-		print "before norm_name"
-		print aln.get_species()
-		aln.norm_name()
-		print "after norm_name"
-		print aln.get_species()
 		dico_aln=aln.get_dico_sp_seq()
 		#print dico_aln
 		sp_list=aln.get_species()
