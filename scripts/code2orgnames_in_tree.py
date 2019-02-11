399d1973256be56f8591bf354218ed9055824558
Florent Lassalle <florentlassalle@hotmail.fr>
2019-02-11 16:18:48 +0000

    change standard species tree parsing function

--- scripts/code2orgnames_in_tree.py
+++ scripts/code2orgnames_in_tree.py
@@ -10,7 +10,7 @@ nfref = sys.argv[2]
 nfout = sys.argv[3]
 
 #~ t = tree2.Node(file=nftreein, unrooted=True)
-t = tree2.read_check_newick(nftreein)
+t = tree2.read_check_newick(nftreein, maxNoBS=-1)
 
 with open(nfref, 'r') as fref:
 	for line in fref:
