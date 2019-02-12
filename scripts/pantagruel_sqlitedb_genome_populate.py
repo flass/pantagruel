75c366a5b582f686b25fd36c058ec5d3fc62df87
Florent Lassalle <florentlassalle@hotmail.fr>
2019-02-12 13:28:53 +0000

    fix parsing of user genome info file; debug

--- scripts/pantagruel_sqlitedb_genome_populate.py
+++ scripts/pantagruel_sqlitedb_genome_populate.py
@@ -5,6 +5,7 @@ import sqlite3
 
 CODepat = re.compile("([A-Z0-9]{3,5}) +[ABEVO] +([0-9]{1,7}): ")
 seqprojpat = re.compile("([^\.]+)\.[1-9]_.+?$")
+assidpat = re.compile("([^\.]+\.[1-9])_.+?$")
 
 def getTableInfos(table, cursor, ommitserial=False):
 	cursor.execute("PRAGMA table_info(%s);"%table)
@@ -209,8 +210,9 @@ def main(dbname, protorfanclust, cdsorfanclust, nfspeclist, nfusergenomeinfo, us
 			if not ('assembly_id' in uginfheader):
 				if usergenomefinalassdir and ('sequencing_project_id' in uginfheader):
 					print "fetching assembly ids from matching values of 'sequencing_project_id' field to folder names in GenBank-like assembly folder: '%s'" %usergenomefinalassdir
-					for assid in os.listdir(usergenomefinalassdir):
-						seqproj = seqprojpat.search(assid).groups()[0]
+					for assidname in os.listdir(usergenomefinalassdir):
+						seqproj = seqprojpat.search(assidname).groups()[0]
+						assid = assidpat.search(assidname).groups()[0]
 						dseqproj2assid[seqproj] = assid
 				else:
 					raise ValueError, "the 'assembly_id' field is missing in file '%s';\n"%nfusergenomeinfo+ \
@@ -222,7 +224,10 @@ def main(dbname, protorfanclust, cdsorfanclust, nfspeclist, nfusergenomeinfo, us
 				code, taxid = (duginfo['locus_tag_prefix'], duginfo['taxid'])
 				fout.write("%s\t%s\n"%(code, taxid))
 				codetaxids.append((code, taxid))
-				dcustomasscode[duginfo.get('assembly_id', dseqproj2assid[duginfo['sequencing_project_id']])] = duginfo['locus_tag_prefix']
+				assid = duginfo.get('assembly_id', dseqproj2assid[duginfo['sequencing_project_id']])
+				code = duginfo['locus_tag_prefix']
+				print assid, ':', code
+				dcustomasscode[assid] = code
 
 	fout.close()
 
