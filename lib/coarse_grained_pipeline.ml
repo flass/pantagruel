ba3e4f5d01929e84517989167846d98622251622
Philippe Veber <philippe.veber@gmail.com>
2019-02-21 11:41:50 +0100

    bistro: retried call to init script

--- lib/coarse_grained_pipeline.ml
+++ lib/coarse_grained_pipeline.ml
@@ -7,24 +7,74 @@ let img = [ docker_image ~account:"pveber" ~name:"pantagruel-tools" ~tag:"latest
 
 let input_dep p = dep (Workflow.input p)
 
-let init ~dbname ~rootdir ~email ~famprefix ~ncbi_assembly_dir ~ncbi_taxonomy_dir ~custom_assembly_dir ~ncbi_assembly_4annot_dir =
-  let r x = Filename.concat rootdir x in
-  Workflow.shell ~descr:"pantagruel.init" [
-    cmd "/pipeline/pantagruel_pipeline_init.sh" ~img [
-      string dbname ;
-      string rootdir ;
-      string "/pantagruel" ;
-      string email ;
-      string famprefix ;
-      string (Option.value ~default:(r"NCBI_assembly") ncbi_assembly_dir) ;
-      string (Option.value ~default:(r"NCBI_taxonomy") ncbi_taxonomy_dir) ;
-      string (Option.value ~default:(r"user_genome") custom_assembly_dir) ;
-      string (Option.value ~default:(r"NCBI_assembly_annot") ncbi_assembly_4annot_dir) ;
+let env xs =
+  list
+    ~sep:"\n"
+    (fun (k, v) -> seq ~sep:"" [ string (sprintf "export %s=" k) ; v ])
+    xs
+
+let environment
+    ?(ptgrepo = string "/pantagruel")
+    ?(email = string "undisclosed")
+    ?(famprefix = string "PANTAG")
+    ?(customassemb = dest // "user_genome")
+    ?(ncbiass = dest // "NCBI/Assembly")
+    ?(refass = dest // "NCBI/Assembly")
+    ?(ncbitax = dest // "NCBI/Taxonomy")
+    ?(chaintype = string "fullgenetree")
+    ?(pseudocoremingenomes = string "")
+    ?(coreseqtype = string "cds")
+    ?(poplgthresh = string "default")
+    ?(popbsthresh = string "default")
+    ?(hpcremoteptgroot = string "none")
+    ~ptgroot ~ptgdbname
+    ()
+  =
+  env [
+    "ptgrepo", ptgrepo ;
+    "email", email ;
+    "famprefix", famprefix ;
+    "customassemb", customassemb ;
+    "ncbiass", ncbiass ;
+    "refass", refass ;
+    "ncbitax", ncbitax ;
+    "chaintype", chaintype ;
+    "pseudocoremingenomes", pseudocoremingenomes ;
+    "coreseqtype", coreseqtype ;
+    "poplgthresh", poplgthresh ;
+    "popbsthresh", popbsthresh ;
+    "hpcremoteptgroot", hpcremoteptgroot ;
+    "ptgroot", ptgroot ;
+    "ptgdbname", ptgdbname ;
+  ]
+
+let script xs = seq ~sep:"\n" (List.map ~f:(seq ~sep:" ") xs)
+
+let init ~ptgdbname ~ptgroot ?email ?famprefix ~ncbi_assembly_dir:_ ~ncbi_taxonomy_dir:_ ~custom_assembly_dir:_ ~ncbi_assembly_4annot_dir:_ () =
+  let env =
+    environment
+      ~ptgdbname ~ptgroot ?email ?famprefix ()
+  in
+  let script = script [
+      [ string "source" ; file_dump env ] ;
+      [ string "/pantagruel/scripts/pipeline/pantagruel_pipeline_init.sh" ] ;
     ]
+  in
+  Workflow.shell ~descr:"pantagruel.init" [
+    cmd "bash" ~img [ file_dump script ]
   ]
 
 let main ~outdir ~ncbi_assembly_dir ~ncbi_taxonomy_dir ~custom_assembly_dir ~ncbi_assembly_4annot_dir () =
-  let w = init ~dbname:"ngono" ~rootdir:"." ~email:"philippe.veber@univ-lyon1.fr" ~famprefix:"NGONO" ~ncbi_assembly_dir ~ncbi_taxonomy_dir ~custom_assembly_dir ~ncbi_assembly_4annot_dir in
+  let w =
+    init
+      ~ptgdbname:(string "ngono")
+      ~ptgroot:dest
+      ~ncbi_assembly_dir
+      ~ncbi_taxonomy_dir
+      ~custom_assembly_dir
+      ~ncbi_assembly_4annot_dir
+      ()
+  in
   Repo.build_main ~outdir ~np:4 ~mem:(`GB 4) Repo.[ item ["delme"] w ]
 
 let command =
