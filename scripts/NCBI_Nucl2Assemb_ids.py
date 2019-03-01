8689ae059a9b54bbfe36e603e1a9905d5647198c
Florent Lassalle <florentlassalle@hotmail.fr>
2019-03-01 15:33:58 +0000

    add utility for querying NCBI and get Assembly ids prior to pantagruel init run (especially when using -L option)

---
+++ scripts/NCBI_Nucl2Assemb_ids.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys, os
+from Bio import Entrez
+
+nfnucid = sys.argv[1]
+Entrez.email =  sys.argv[2]
+
+with open(nfnucid, 'r') as fnucid:
+	lnucid = [line.rstrip('\n') for line in fnucid]
+	#~ header = fnucid.readline().rstrip('\n').split('\t')
+	#~ ldnucid = [dict(zip(header, line.rstrip('\n').split('\t'))) for line in fnucid]
+
+for nucid in lnucid:
+#~ for dnucid in ldnucid:
+	#~ nucid = dnucid['Nuc_accession_long']
+	print nucid,
+	#~ if nucid.startswith('NZ_'):
+		#~ nucid = nucid.split('_', 1)[0]
+	try:
+		erecord = Entrez.read(Entrez.elink(db="assembly", dbfrom="nucleotide", id=nucid))
+		assUid = erecord[0]['LinkSetDb'][0]['Link'][0]['Id']
+		esum = Entrez.read(Entrez.esummary(db='assembly', id=assUid))
+		print esum['DocumentSummarySet']['DocumentSummary'][0]['AssemblyAccession']
+	except IndexError, e:
+		#~ bioprojid = dnucid['Bioproject']
+		#~ erecord = Entrez.read(Entrez.elink(db="assembly", dbfrom="bioproject", id=bioprojid, idtype='acc'))
+		#~ assUid = erecord[0]['LinkSetDb'][0]['Link'][0]['Id']
+		#~ esum = Entrez.read(Entrez.esummary(db='assembly', id=assUid))
+		#~ print esum['DocumentSummarySet']['DocumentSummary'][0]['AssemblyAccession']
+		print ''
