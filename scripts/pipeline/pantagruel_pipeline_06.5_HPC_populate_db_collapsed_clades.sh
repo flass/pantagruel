7d55c9166902ce9ee254a675645c337b58d4d6c0
Florent Lassalle <florentlassalle@hotmail.fr>
2019-03-09 16:57:11 +0000

    remove uselessHPC options ; add error message ('not implemented yet') when collapsing is not enabled

--- scripts/pipeline/pantagruel_pipeline_06.5_HPC_populate_db_collapsed_clades.sh
+++ scripts/pipeline/pantagruel_pipeline_06.5_HPC_populate_db_collapsed_clades.sh
@@ -9,47 +9,18 @@
 
 # Copyright: Florent Lassalle (f.lassalle@imperial.ac.uk), 15 Jan 2019
 
-if [ -z "$1" ] ; then echo "missing mandatory parameter: pantagruel config file" ; echo "Usage: $0 ptg_env_file [ncpus=12] [mem=124gb] [walltimehours=24] [parallelflags='']" ; exit 1 ; fi
+if [ -z "$1" ] ; then echo "missing mandatory parameter: pantagruel config file" ; echo "Usage: $0 ptg_env_file" ; exit 1 ; fi
 envsourcescript="$1"
 source ${envsourcescript}
-
-if [ ! -z "$2" ] ; then
-  ncpus="$2"
-else
-  ncpus=12
-fi
-if [ ! -z "$3" ] ; then
-  mem="$3"
-else
-  mem=124gb
-fi
-if [ ! -z "$4" ] ; then
-  wth="$4"
-else
-  wth=24
-fi
-if [ ! -z "$5" ] ; then
-  parallelflags=":$5"
-  # for instance, parallelflags="mpiprocs=1:ompthreads=8"
-  # will force the use of OpenMP multi-threading instead of default MPI parallelism
-  # note this is not standard and might not be the default on your HPC system;
-  # also the flags may be different depending on the HPC system config.
-  # you should get in touch with your system admins to know the right flags
-else
-  parallelflags=""
-fi
-
-
 
 ################################################################################
 ## 06.5 Populate database withh all gene tree results
 ################################################################################
 
 if [[ "${chaintype}" == 'fullgenetree' ]] ; then
-  #### OPTION A2: 
-  echo "Error: not implemented yet:"
-  echo "nothing to do; exit now"
-  exit 0
+  #### OPTION A2: pretty much nothing to do
+  echo "Error: not implemented yet exit now"
+  exit 1
   
   #### end OPTION A2: 
 else
