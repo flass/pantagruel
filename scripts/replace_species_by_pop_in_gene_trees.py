505176302567bb9f3a7e8a86fac935d9f4ed62c7
Florent Lassalle <florentlassalle@hotmail.fr>
2019-03-11 18:30:19 +0000

    debug to allow for case where species codes contain '_' characters

--- scripts/replace_species_by_pop_in_gene_trees.py
+++ scripts/replace_species_by_pop_in_gene_trees.py
@@ -713,8 +713,8 @@ def mapPop2GeneTree(nfingtchain1, dircons, dirout, method, spetree, poptree, dsp
 				elif isinstance(newlaborst, BaseTree.TreeElement):
 					newlabs = [tip.name for tip in newlaborst.get_terminals()]
 				for newlab in newlabs:
-					newlabpop, fam, ngtag = newlab.rsplit('_',2)
-					dpoptyperepcount.setdefault(newlabpop, {rt:0 for rt in reptypes})[ngtag[:2]] += 1
+					newlabpop, fam, ngtag = newlab.rsplit('-'+oldlab, 1)[0].rsplit('_',2)
+					dpoptyperepcount.setdefault(newlabpop, {rt:0 for rt in reptypes})[ngtag] += 1
 					foutreflab.write('\t'.join((oldlab, newlab))+'\n')
 	if dpoptyperepcount or ((not os.path.exists(nfoutlabpoplab)) or (not reuseOutput)):
 		with open(nfoutlabpoplab, 'w') as foutlabpoplab:
