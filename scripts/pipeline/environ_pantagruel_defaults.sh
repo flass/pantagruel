f3f3097c1920c6a387fad4fecd798c7cbb19acd7
Florent Lassalle <florentlassalle@hotmail.fr>
2019-05-01 11:57:37 +0100

    document config file templates and sources

--- scripts/pipeline/environ_pantagruel_defaults.sh
+++ scripts/pipeline/environ_pantagruel_defaults.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+# Prokka annotation parameters (only relevant if custom genome assemblies are provided):
+export assembler="somesoftware"
+export seqcentre="somewhere"
+export refgenus="Reference"
+# species tree inference parameters
+export ncorebootstrap=200
+# gene tree inference parameters
+export mainresulttag='rootedTree'
+# gene trees collapsing DEFAULT values (used when -C option is not present in init call)
 export cladesuppdef=70
 export subcladesuppdef=35
 export criteriondef='bs'
 export withinfundef='median'
+# gene tree/species tree reconciliation inference parameters
 export ALEalgo='ALEml'
 export recsamplesize=1000
+# gene tree/species tree reconciliation parsing parameters for co-evolution analysis
 export evtypeparse='ST'
 export minevfreqparse=0.1
 export minevfreqmatch=0.5
 export minjoinevfreqmatch=1.0
 export maxreftreeheight=0.25
-export ncorebootstrap=200
-export mainresulttag='rootedTree'
-# only relevant if user-defined genomes are provided
-export assembler="somesoftware"
-export seqcentre="somewhere"
-export refgenus="Reference"
--- scripts/pipeline/environ_pantagruel_template.sh
+++ scripts/pipeline/environ_pantagruel_template.sh
@@ -54,23 +54,28 @@ export hpcremoteptgroot='REPLACEhpcremoteptgroot'          # if not empty nor 'n
 source ${ptgscripts}/pipeline/environ_pantagruel_defaults.sh
 # these defalts can be overriden by uncommenting the relevant line below and editing the variable's value
 # default values are:
+# Prokka annotation parameters (only relevant if custom genome assemblies are provided):
+#~ export assembler="somesoftware"
+#~ export seqcentre="somewhere"
+#~ export refgenus="Reference"
+# species tree inference parameters
+#~ export ncorebootstrap=200
+# gene tree inference parameters
+#~ export mainresulttag='rootedTree'
+# gene trees collapsing DEFAULT values (used when -C option is not present in init call)
 #~ export cladesuppdef=70
 #~ export subcladesuppdef=35
 #~ export criteriondef='bs'
 #~ export withinfundef='median'
+# gene tree/species tree reconciliation inference parameters
 #~ export ALEalgo='ALEml'
 #~ export recsamplesize=1000
+# gene tree/species tree reconciliation parsing parameters for co-evolution analysis
 #~ export evtypeparse='ST'
 #~ export minevfreqparse=0.1
 #~ export minevfreqmatch=0.5
 #~ export minjoinevfreqmatch=1.0
 #~ export maxreftreeheight=0.25
-#~ export ncorebootstrap=200
-#~ export mainresulttag='rootedTree'
-#~ # only relevant if user-defined genomes are provided
-#~ export assembler="somesoftware"
-#~ export seqcentre="somewhere"
-#~ export refgenus="Reference"
 
 # secondary vars are defined based on the above
 source ${ptgscripts}/pipeline/environ_pantagruel_secondaryvars.sh
