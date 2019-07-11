4a5bac2451e1e9216de39856b6e400ebf4e48302
Florent Lassalle <florentlassalle@hotmail.fr>
2019-07-11 12:50:55 +0100

    utility for summarizing occurrence of collapsed clades = gene allele among populations

---
+++ scripts/summarize_CCallelesharing_betweenpops.py
@@ -0,0 +1,34 @@
+python
+
+import sys, os
+
+
+nflnfpopref = sys.argv[1]
+nfpops = sys.argv[2]
+nfout = sys.argv[3]
+
+with open(nflnfpopref, 'r') as flnfpopref:
+	lnfpopref = [line.rstrip('\n') for line in flnfpopref]
+
+lpops = []
+with open(nfpops, 'r') as fpops:
+	for line in fpops:
+		if line.startswith('#'): continue
+		lpops.append(line.split('\t')[0])
+
+fout = open(nfout, 'w')
+fout.write('\t'.join(['']+lpops)+'\n')
+
+for nfpopref in lnfpopref:
+	fam = os.path.basename(nfpopref).split('-')[0]
+	with open(nfpopref, 'r') as fpopref:
+		for line in fpopref:
+			colclade, spopfreqs = line.rstrip('\n').split('\t', 1)
+			famcc = fam+'-'+colclade
+			lfamcc.append(famcc)
+			dpopfreqs = {p:float(f) for p, f in [pf.split(':') for pf in spopfreqs.split('\t')]}
+			fout.write('\t'.join([famcc]+[str(dpopfreqs.get(pop, '0.0')) for pop in lpops])+'\n')
+
+fout.close()
+		
+		
