6ab91a32e335f2a229f7837365a678055c13e9b7
Florent Lassalle <florentlassalle@hotmail.fr>
2019-07-25 14:36:55 +0100

    avoid reporting absence of *_assembly_stats.txt files

--- scripts/groom_refseq_data.sh
+++ scripts/groom_refseq_data.sh
@@ -15,17 +15,14 @@ patseqteq="Sequencing technology"
 patcontig="all\tall\tall\tall\tcontig-"	
 # assembly status
 sedfilter0="s@\(.\+\)/.\+${filetag}:# ${patasslev}: \(.\+\)@\1\t\2@g"
-grep "$patasslev" */*${filetag} | sed -e "$sedfilter0" | sed -e 's/\r//g' > ${stats}/assembly_level.tab
-# assembly status
-sedfilter0="s@\(.\+\)/.\+${filetag}:# ${patasslev}: \(.\+\)@\1\t\2@g"
-grep "$patasslev" */*${filetag} | sed -e "$sedfilter0" | sed -e 's/\r//g' > ${stats}/assembly_level.tab
+grep "$patasslev" ./*/*${filetag} 2> /dev/null | sed -e "$sedfilter0" | sed -e 's/\r//g' > ${stats}/assembly_level.tab
 # sequencing technology
 sedfilter1="s@\(.\+\)/.\+${filetag}:# ${patseqteq}: \(.\+\)@\1\t\2@g"
 sedfilter2="s@\(.\+\)/.\+${filetag}@\1\tNA@g"
-grep "$patseqteq" */*${filetag} | sed -e "$sedfilter1" | sed -e 's/\r//g' > ${stats}/sequencing_technology.tab
-grep -L "$patseqteq" */*${filetag} | sed -e "$sedfilter2" | sed -e 's/\r//g' >> ${stats}/sequencing_technology.tab
+grep "$patseqteq" ./*/*${filetag} 2> /dev/null | sed -e "$sedfilter1" | sed -e 's/\r//g' > ${stats}/sequencing_technology.tab
+grep -L "$patseqteq" ./*/*${filetag} 2> /dev/null | sed -e "$sedfilter2" | sed -e 's/\r//g' >> ${stats}/sequencing_technology.tab
 # contigs
 for info in count N50 ; do
   sedfilter3="s@\(.\+\)/\1${filetag}:${patcontig}${info}\t\(.\+\)@\1\t\2@g"
-  grep -P $patcontig */*${filetag} | sed -e $sedfilter3 | grep -v $filetag > ${stats}/contig-${info}
+  grep -P $patcontig ./*/*${filetag} 2> /dev/null | sed -e $sedfilter3 | grep -v $filetag > ${stats}/contig-${info}
 done
