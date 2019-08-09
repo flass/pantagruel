58d022f1778b26b5c439dd838df91bf4208176c7
Florent Lassalle <florentlassalle@hotmail.fr>
2019-08-09 16:53:42 +0100

    remove silly test

--- scripts/make_adjacency_file_from_trees.py
+++ scripts/make_adjacency_file_from_trees.py
@@ -18,17 +18,18 @@ dspe2genes = {}
 for nfgt in lnfgt:
 	gt = tree2.read_newick(nfgt)
 	genes = gt.get_leaf_labels()
-	s = ''
-	for i, g in enumerate(genes):
-		s0 = s
+	for g in genes:
 		# assume gene labels to follow the syntax SPECIES_1234
 		s, n = g.split('_')
-		if i>0: assert s0 == s
 		dspe2genes.setdefault(s, []).append(int(n))
+
+lspe = dspe2genes.keys()
+lspe.sort()
+print "%d species: %s, ..."%(len(lspe), ', '.join(lspe[:5]))
 
 # make adjacency file 
 with open(nfout, 'w') as fout:
-	for s in dspe2genes:
+	for s in lspe:
 		lg = dspe2genes[s]
 		lg.sort()
 		for k in range(len(lg)-1):
