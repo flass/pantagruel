d552e5228bb9041f40b7f2f88652b2980ec85e06
Florent Lassalle <fl4@sanger.ac.uk>
2019-09-03 12:07:29 +0100

    do not attempt download when source folder not found

--- scripts/fetch_refseq_assemblies_from_list.sh
+++ scripts/fetch_refseq_assemblies_from_list.sh
@@ -52,13 +52,13 @@ fetchass (){
   echo -e "fetch ${assdir}\n"
   # then download
   if [ -z "${suf}" ] ; then
-	# the target folder
+    # the target folder
     lftp ${openparam} -e "set ftp:use-feat false ; mirror -X *_assembly_structure ${para} ${assdir} ; quit"
   else
-	# the specific target files
-	mkdir -p ${outdir}/${fullass}/
-	lftp ${openparam} -e "set ftp:use-feat false ; mget -O ${fullass} ${assdir}/${fullass}${suf} ; get -O ${fullass} ${assdir}/md5checksums.txt ; quit"
-	grep ${fullass}${sufpat} ${outdir}/${fullass}/md5checksums.txt > ${outdir}/${fullass}/md5checksums${suftxt}.txt
+    # the specific target files
+    mkdir -p ${outdir}/${fullass}/
+    lftp ${openparam} -e "set ftp:use-feat false ; mget -O ${fullass} ${assdir}/${fullass}${suf} ; get -O ${fullass} ${assdir}/md5checksums.txt ; quit"
+    grep ${fullass}${sufpat} ${outdir}/${fullass}/md5checksums.txt > ${outdir}/${fullass}/md5checksums${suftxt}.txt
   fi
   ls -l ${outdir}/${fullass}/
   cd ${outdir}/${fullass}/
@@ -83,21 +83,23 @@ for ass in $(cat ${asslist}) ; do
   if [ -z ${assdir} ] ; then
     echo "could not find folder of accession '${ass}' on NCBI FTP when looking for folder matching '${assloc}/${ass}*' ; exit now" >&2
     if [ -z ${forcemode} ] ; then exit 1 ; fi
-  fi
-  fullass=$(basename ${assdir})
-  if [ -e ${outdir}/${fullass}/md5checksums${suftxt}.txt ] ; then
-    cd ${outdir}/${fullass}/
-    md5sum -c md5checksums${suftxt}.txt
-	md5stat=${?}
-    cd ${outdir}/
-    if [ ${md5stat} -gt 0 ] ; then
-	  echo "the folder of accession '${ass}' already exists locally, but the file set is not complete; try and repeat the transfer"
-	  fetchass ${assdir}
-    else
-      echo "assembly ${fullass} already present in ${outdir}/"
-    fi
   else
-    fetchass ${assdir}
+      fullass=$(basename ${assdir})
+      if [ -e ${outdir}/${fullass}/md5checksums${suftxt}.txt ] ; then
+        cd ${outdir}/${fullass}/
+        md5sum -c md5checksums${suftxt}.txt
+        md5stat=${?}
+        cd ${outdir}/
+        if [ ${md5stat} -gt 0 ] ; then
+          echo "the folder of accession '${ass}' already exists locally, but the file set is not complete; try and repeat the transfer"
+          fetchass ${assdir}
+        else
+          echo "assembly ${fullass} already present in ${outdir}/"
+        fi
+      else
+        fetchass ${assdir}
+      fi
   fi
+  
   echo -e "${ass}: done\n"
 done
