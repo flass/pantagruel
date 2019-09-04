f490148c8437b47bea7594f8a51933c739b2d401
Florent Lassalle <fl4@sanger.ac.uk>
2019-09-04 09:52:34 +0100

    fix parsing of rooted tree

--- scripts/putidentseqbackintree.py
+++ scripts/putidentseqbackintree.py
@@ -27,33 +27,45 @@ if nfidentseqs:
 			dseq.setdefault(lsp[0], []).append(lsp[1])
 
 if nfrefroottree:
-	refroottree = tree2.read_check_newick(nfrefroottree, maxNoBS=-1, verbose=False)
-	subrootchildren = refroottree.get_children()
-	# map sub-root clades in reference tree to the input tree
-	mappedclades = [intree.map_to_node(src.get_leaf_labels()) for src in subrootchildren]
-	# find outgroup index as the one not being root
-	for i, mc in enumerate(mappedclades):
-		if not mc.is_root(): break
+	refroottree = tree2.read_check_newick(nfrefroottree)
+	
+	if not refroottree.is_bifurcated():
+		raise ValueError, "the provided 'rooted' tree is not bifurcated"
+	nnodenobs = tree2.checkBS(refroottree, maxNoBS=-1)
+	if nnodenobs <4:
+		# the rooted tree can be used as is
+		intree = refroottree
 	else:
-		raise IndexError, "Could not find in '%s' the basal clade forming an outgroup in '%s'"%(nfintree, nfrefroottree)
-	outgroup = mappedclades[i]
-	# branch length under the root such as: [bl(outgroup), bl(other clade)]
-#	subrootbrlens = [subrootchildren[t].lg() for  t in (i, 1-i)]
-	# re-root the input tree as in the reference
-#	intree.newOutgroup(outgroup, branch_lengths=subrootbrlens)
-	intree.resolveNode([outgroup])
-	intree = intree.go_root()
-	assert intree.is_bifurcated()
-	if subrootchildren[0].lg() > 0:
-		# correct the branch lengths
-		for srnode in intree.get_children():
-			for src in subrootchildren:
-				# find the matching reference node
-				if set(src.get_leaf_labels()) ==  set(srnode.get_leaf_labels()):
-					srnode.set_lg(src.lg())
-					break
-			else:
-				raise IndexError, "could not mnatch the reference in input tree nodes"
+		print "must transfer supports from the tree with bipartitions"
+		subrootchildren = refroottree.get_children()
+		# map sub-root clades in reference tree to the input tree
+		mappedclades = [intree.map_to_node(src.get_leaf_labels()) for src in subrootchildren]
+		# find outgroup index as the one not being root
+		for i, mc in enumerate(mappedclades):
+			if not mc.is_root():
+				break
+		else:
+			raise IndexError, "Could not find in '%s' the basal clade forming an outgroup in '%s'"%(nfintree, nfrefroottree)
+		outgroup = mappedclades[i]
+		outfat = outgroup.go_father()
+
+		# branch length under the root such as: [bl(outgroup), bl(other clade)]
+		subrootbrlens = [subrootchildren[t].lg() for  t in (i, 1-i)]
+		# re-root the input tree as in the reference
+		intree.newOutgroup(outgroup, branch_lengths=subrootbrlens)
+		intree.resolveNode([outgroup])
+		intree = intree.go_root()
+		assert intree.is_bifurcated()
+		if subrootchildren[0].lg() > 0:
+			# correct the branch lengths
+			for srnode in intree.get_children():
+				for src in subrootchildren:
+					# find the matching reference node
+					if set(src.get_leaf_labels()) ==  set(srnode.get_leaf_labels()):
+						srnode.set_lg(src.lg())
+						break
+				else:
+					raise IndexError, "could not mnatch the reference in input tree nodes"
 
 for nrseq in dseq:
 	leaf = intree[nrseq]
