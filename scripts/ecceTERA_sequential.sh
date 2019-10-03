29b3587eeb498295be9d9271633c5b48e594ca30
Florent Lassalle <fl4@sanger.ac.uk>
2019-10-03 10:23:13 +0100

    fix output dir variable definition

--- scripts/ecceTERA_sequential.sh
+++ scripts/ecceTERA_sequential.sh
@@ -246,7 +246,7 @@ for nfchain in $(cat $tasklist) ; do
     fi
     promptdate
   
-    runopt="$(makerunopt ${stree} ${bnchain} ${outputdir})"
+    runopt="$(makerunopt ${stree} ${bnchain} ${resultdir})"
     teracmd="ecceTERA ${runopt} ${commonopt} ale=1 amalgamate=1"
     echo "# ${teracmd}"
     ${teracmd}
@@ -258,7 +258,7 @@ for nfchain in $(cat $tasklist) ; do
 	  # converts it from Nexus to Newick
 	  python convert_mrbayes_constree_nex2nwk.py ${bnchain}
 	fi
-	runopt="$(makerunopt ${stree} ${bnchain}.nwk ${outputdir})"
+	runopt="$(makerunopt ${stree} ${bnchain}.nwk ${resultdir})"
 	teracmd="ecceTERA ${runopt} ${commonopt} ${collapseopts}"
 	echo "# ${teracmd}"
 	${teracmd}
