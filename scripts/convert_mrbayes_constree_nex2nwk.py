1490b26f3b003457994b8f1f33a4dd61d9055f98
Florent Lassalle <fl4@sanger.ac.uk>
2019-10-03 10:35:56 +0100

    fix indent

--- scripts/convert_mrbayes_constree_nex2nwk.py
+++ scripts/convert_mrbayes_constree_nex2nwk.py
@@ -4,11 +4,12 @@ import tree2, sys
 sys.setrecursionlimit(10000)
 nfcongt = sys.argv[1]
 if nfcongt.endswith('.nwk'):
-  congt = tree2.read_newick(nfcongt)
+	congt = tree2.read_newick(nfcongt)
 else:
-  congt = tree2.read_nexus(nfcongt, returnDict=False, allLower=False)[0]
+	congt = tree2.read_nexus(nfcongt, returnDict=False, allLower=False)[0]
+
 for node in congt:
-  if not node.bs():
-    node.set_bs(0.01)
+	if not node.bs():
+    	node.set_bs(0.01)
 
 congt.write_newick('%s.nwk'%nfcongt)
