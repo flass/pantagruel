415b44c0c405a05b291364b74f6826e535421d76
Florent Lassalle <fl4@sanger.ac.uk>
2019-10-31 11:15:52 +0000

    extend scope of lsfullpath.py that previously only explored folder through globing of patterns such as 'd/*' - which was all that it was used for so far in pantagruel pipeline scripts; now it also list files within folders specified by patterns such as 'd/' or simply 'd' (if 'd' is a folder)

--- scripts/lsfullpath.py
+++ scripts/lsfullpath.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
-import sys, glob
+import sys
+import glob
+import os
 
 for target in sys.argv[1:]:
-	lpath = glob.iglob(target)
+	if (('*' in target) or (('[' in target.replace('\[', '')) and (']' in target.replace('\]', '')))):
+		lpath = glob.iglob(target)
+	elif os.path.isdir(target):
+		lpath = os.listdir(target)
+	else:
+		lpath = target
 	for f in lpath:
 		print f
