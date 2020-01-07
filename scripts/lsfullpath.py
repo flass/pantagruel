6976dd00bacf72b6b7e2b84fdd7c47dda0da1c77
Florent Lassalle <fl4@sanger.ac.uk>
2020-01-07 20:15:31 +0000

    fix!

--- scripts/lsfullpath.py
+++ scripts/lsfullpath.py
@@ -8,7 +8,7 @@ for target in sys.argv[1:]:
 	if (('*' in target) or (('[' in target.replace('\[', '')) and (']' in target.replace('\]', '')))):
 		lpath = glob.iglob(target)
 	elif os.path.isdir(target):
-		lpath = [os.path.jpin(target, nf) for nf in os.listdir(target)]
+		lpath = [os.path.join(target, nf) for nf in os.listdir(target)]
 	else:
 		lpath = [target]
 	for f in lpath:
