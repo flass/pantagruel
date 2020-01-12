be02647ed6c74d71cec906d660e09708e0883732
Florent Lassalle <fl4@sanger.ac.uk>
2020-01-12 20:03:10 +0000

    change interface towards using only alignements as input

--- scripts/make_generax_family_file.py
+++ scripts/make_generax_family_file.py
@@ -1,49 +1,60 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-import sys, os, glob
+import sys, os, glob, getopt
 import tree2
 
+def usage():
+	s = '[need help message here]'
+	return s
+
+opts, args = getopt.getopt(sys.argv[1:], 'a:o:hv', ['alignments=', 'gene-trees=', 'model=', 'out='])
+dopt = dict(opts)
+if ('-h' in dopt) or ('--help' in dopt):
+	print usage()
+	sys.exit(0)
+
 gttag = '-Gtree.nwk'
+alitag = 'aln'
 
-dirgt = sys.argv[1]
-nfout = sys.argv[2]
-if len(sys.argv) > 3:
-	model = sys.argv[3]
-else:
-	model = 'GTR+G'
+dirali = dopt.get('-a', dopt.get('--alignments'))
+dirgt = dopt.get('--gene-trees')
+model = dopt.get('model', 'GTR+G')
+nfout = dopt.get('-o', dopt.get('--out'))
 
-#with open(nflnfgt, 'r') as flnfgt:
-#	lnfgt = [line.rstrip('\n') for line in flnfgt]
+if not nfout:
+	raise ValueError, "you must specify an ouput file with -o|--out"
+if not dirali:
+	raise ValueError, "you must specify an input alignment folder with -a|--alignments"
+
+lnfali = glob.glob('%s/*%s'%(dirali, gttag))
 lnfgt = glob.glob('%s/*%s'%(dirgt, gttag))
 
 fout = open(nfout, 'w')
 fout.write('[FAMILIES]\n')
 
-for nfgt in lnfgt:
-	bngt = os.path.basename(nfgt)
+for nfali in lnfali:
+	bnali = os.path.basename(nfali)
 #	dirgt = os.path.dirname(nfgt)
-	bnrad1 = bngt.rsplit('.', 1)[0]
-	bnrad2 = bnrad1.rsplit('-', 1)[0]
-	if bnrad2.endswith('-replaced'): bnrad3 = bnrad2.rsplit('-', 1)[0]
-	else: bnrad3 = bnrad2
-	fam = bnrad1.split('-')[0]
-	# assume everything is in the same folder
-	bnaln = bnrad2+'.aln'
-#	bnst = bnrad3+'-Stree.nwk'
-	bnmap = bnrad2+'.link'
-	nfmap = os.path.join(dirgt, bnmap)
-	with open(nfmap, 'w') as fmap:
-		# generate the mapping file
-		gt = tree2.read_newick(nfgt)
-		for leaflab in gt.get_leaf_labels():
-			fmap.write('%s\t%s\n'%(leaflab, leaflab.split('_', 1)[0]))
+	fam = bnali.rsplit('.')[0]
+	if dirgt:
+		nfgt = glob.glob("%s/*%s*%s"%(dirgt, fam, gttag))
+		bnmap = bnrad2+'.link'
+		nfmap = os.path.join(dirgt, bnmap)
+		with open(nfmap, 'w') as fmap:
+			# generate the mapping file
+			gt = tree2.read_newick(nfgt)
+			for leaflab in gt.get_leaf_labels():
+				fmap.write('%s\t%s\n'%(leaflab, leaflab.split('_', 1)[0]))
+	else:
+		nfmap = None
+		nfgt = None
 	
 	# write out
 	fout.write('- %s\n'%fam)
-	fout.write('mapping = %s\n'%(os.path.join(dirgt, bnmap)))
 	fout.write('alignment = %s\n'%(os.path.join(dirgt, bnaln)))
 	fout.write('subst_model = %s\n'%(model))
-	fout.write('starting_gene_tree = %s\n'%(nfgt))
+	if nfgt: fout.write('starting_gene_tree = %s\n'%(nfgt))
+	if nfmap: fout.write('mapping = %s\n'%(os.path.join(dirgt, bnmap)))
 	
 fout.close()
