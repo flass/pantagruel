f7f7f1efb602b4f376bb78ef4def86e57beb6a0c
Florent Lassalle <fl4@sanger.ac.uk>
2020-04-20 15:10:47 +0100

    add little bit more verbosity

--- scripts/pipeline/pantagruel_pipeline_00_fetch_data.sh
+++ scripts/pipeline/pantagruel_pipeline_00_fetch_data.sh
@@ -318,7 +318,8 @@ if [ ! -z "${customassemb}" ] ; then
           done
           cp ${contigs}/${allcontigs} ${annotgff[0]/gff/fna}
           python2.7 ${ptgscripts}/GFFGenomeFasta2GenBankCDSProtFasta.py ${annotptggff} ${annotgff[0]/gff/fna}
-          checkexec "something went wrong when generating the GenBank flat file from GFF file ${annotptggff}" "succesfuly generated the GenBank flat file from GFF file ${annotptggff}"
+		  sgfin="the GenBank flat file from GFF file '${annotptggff}' and fasta file '${annotgff[0]/gff/fna}'"
+          checkexec "something went wrong when generating ${sgfin}" "succesfuly generated ${sgfin}"
         fi
         annotfna=($(ls ${annot}/${gproject}/*.fna))
         annotgbk=($(ls ${annot}/${gproject}/*.gbk 2> /dev/null))
