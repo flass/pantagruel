4f00f7fd8a7ee211b515d3d9f1241deeef76bf6b
Florent Lassalle <fl4@sanger.ac.uk>
2020-04-21 16:39:05 +0100

    allow for GFF files without gene features; look for locus_tag info directly into the CDS field as a backup

--- scripts/allgenome_gff2db.py
+++ scripts/allgenome_gff2db.py
@@ -98,8 +98,10 @@ def compileFeatures(fgff, dfout, dgenbankcdsids, dgeneloctag, dgenenchild, diden
 		seqreg, annottype, beg, end, strand, desc = parseGFFline(line)
 		if annottype in annottype2fouttag:
 			lineoutend = []
-			parentgene = desc['Parent']
-			locustag = dgeneloctag[parentgene]
+			locustag = None
+			locustag = dgeneloctag.get(desc.get('Parent'))
+			if not locustag: locustag = desc.get('locus_tag')
+			if not locustag: raise ValueError, "cannot find locus tag information either through the parent gene (%s) or directly from the descritption field:\n%s"%(repr(parentgene), repr(desc))
 			# need to buffer extraction of entries as several lines can relate to the same gene in case of CDS/RNA in several segments (due  to introns, framshifts, ...)
 			if annottype=="CDS":
 				productid = desc.get('protein_id', '').split('|')[-1]	# to account for Prokka-style annotation that prepends 'gnl|Sequencing_Centre' to the protein_id
