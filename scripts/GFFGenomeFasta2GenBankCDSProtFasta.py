a962be918940e09943699eafc69ad5d561cfb281
Florent Lassalle <fl4@sanger.ac.uk>
2020-04-21 13:17:06 +0100

    fix to re-introduce strain informations lost in the file format transformation

--- scripts/GFFGenomeFasta2GenBankCDSProtFasta.py
+++ scripts/GFFGenomeFasta2GenBankCDSProtFasta.py
@@ -14,15 +14,43 @@ from Bio import SeqIO
 from Bio.Alphabet import generic_dna
 from BCBio import GFF
 
-def main(gff_file, fasta_file):
+def parse_strain_info(straininfo_file):
+	with open(straininfo_file, 'r') as fin:
+		header = fin.readline().rstrip('\n').split('\t')
+		dstraininfo = {}
+		for line in fin:
+			lsp = line.rstrip('\n').split('\t')
+			straininfo = dict(zip(header, lsp))
+			assid = straininfo['assembly_id']
+			dstraininfo[assid] = straininfo
+	return dstraininfo
+
+def main(gff_file, fasta_file, straininfo_file=None):
+	gid = os.path.splitext(gff_file)[0]
 	fasta_input = SeqIO.to_dict(SeqIO.parse(fasta_file, "fasta", alphabet=generic_dna))
 	genome = list(GFF.parse(gff_file, fasta_input))
+	if straininfo_file: straininfo = parse_strain_info(straininfo_file).get(gid)
+	else: straininfo = {}
+	# enforce alphabet (bug in GFFParser)
+	for seqrec in genome:
+		seqrec.seq.alphabet = generic_dna
+		if straininfo:
+			for feature in seqrec.features:
+				if feature.type=='region':
+					species = straininfo['genus']+' '+straininfo['species'].strip()
+					if not 'organism' in feature.qualifiers:
+						sorg = species
+						if straininfo['strain']: sorg += ' str. '+straininfo['strain']
+						feature.qualifiers['organism'] = [sorg]
+					if not 'species' in feature.qualifiers:
+						feature.qualifiers['organism'] = [species]
+					break # the for feature loop
 	# output Genbank
-	with open("%s.gbk" % os.path.splitext(gff_file)[0], 'w') as gbout_file:
+	with open("%s.gbk" % gid, 'w') as gbout_file:
 		SeqIO.write(genome, gbout_file, "genbank")
 	# output CDS and Proteins
-	cdsout_file = open("%s.ffn" % os.path.splitext(gff_file)[0], 'w')
-	protout_file = open("%s.faa" % os.path.splitext(gff_file)[0], 'w')
+	cdsout_file = open("%s.ffn" % gid, 'w')
+	protout_file = open("%s.faa" % gid, 'w')
 	for genomerecord in genome:
 		for feature in genomerecord.features:
 			if feature.type=='CDS':
