fad94fef5c461220a7687e873ba0837a6c38610d
Florent Lassalle <fl4@sanger.ac.uk>
2020-04-23 19:31:36 +0100

    fixes + add filtering by event type (5th optional argument)

--- scripts/gene_family_co-evolution.r
+++ scripts/gene_family_co-evolution.r
@@ -1,20 +1,44 @@
 #/usr/bin/env Rscript --vanilla --no-save
-library(parallel)
+library('parallel')
+library('RColorBrewer')
 
 maxfreq = 1000
-ncpus = 8
+ncpus = detectCores()
 
 cargs = commandArgs(trailingOnly=T)
 
-lnffamevents = readLines(cargs[1])
-fams = sapply(lnffamevents, function(p){ strplit(basename(p), split='_samples.nhx')[[1]][1] })
+nflnffamevents = cargs[1]
+nffamsizes = cargs[2]
+nftestfams = cargs[3]
+outprefix = cargs[4]
+if (length(cargs)>4){
+	only.eventtypes = strsplit(cargs[5], split=',')[[1]] 
+}else{
+	only.eventtypes = c('D', 'T', 'S', 'L')
+}
+outtag = paste(only.eventtypes, collapse='')
+
+lnffamevents = readLines(nflnffamevents)
+fams = sapply(lnffamevents, function(p){ strsplit(basename(p), split='_samples.nhx')[[1]][1] })
 F = length(fams)
 
-famsizes = read.table(cargs[2], sep='\t', h=F, col.names=c('fam', 'size'))
+famsizes = read.table(nffamsizes, sep='\t', h=F, col.names=c('fam', 'size'))
 
-lfameventtabs = lapply(lnffamevents, read.table, sep='\t', h=F, col.names=c('type', 'location', 'donor', 'freq'))
+testfams = readLines(nftestfams)
+T = length(testfams)
+
+lfameventtabs = lapply(lnffamevents, function(nffamevents){
+	famevents = read.table(nffamevents, sep='\t', h=F, col.names=c('type', 'location', 'donor', 'freq'))
+	famevents[famevents$type %in% only.eventtypes,]
+})
 names(lfameventtabs) = fams
+
+# check data consistency
 stopifnot(all(sapply(fams, function(fam){ fam %in% famsizes$fam })))
+stopifnot(all(sapply(testfams, function(fam){ fam %in% famsizes$fam })))
+stopifnot(all(sapply(testfams, function(fam){ fam %in% fams })))
+
+controlfams = setdiff(fams, testfams)
 
 # truncate aggregated counts of multiple gene tree events with the same species tree address
 for (fameventtab in lfameventtabs){
@@ -23,12 +47,13 @@ for (fameventtab in lfameventtabs){
 
 rowids = 1:(F-1)
 lfamcoevol = mclapply(rowids, function(a){
-	colids = (a+1):min(a+1, F)
+	colids = (a+1):F
 	partialrow = sapply(colids, function(b){
+#		cat(sprintf("\r# %d-%d (%s vs. %s)\t\n", a, b, fams[a], fams[b]))
 		cotab = merge(lfameventtabs[[a]], lfameventtabs[[b]], by=1:3)
 		cotab$coev = sqrt(cotab$freq.x * cotab$freq.y) / maxfreq
 		# sum and scale by gene tree size
-		Gtreesizes = (famsizes$size[famisizes$fam %in% fams[c(a, b)]] * 2) - 2
+		Gtreesizes = (famsizes$size[famsizes$fam %in% fams[c(a, b)]] * 2) - 2
 		return( sum(cotab$coev) / min(Gtreesizes) )
 	})
 	names(partialrow) = fams[colids]
@@ -40,14 +65,47 @@ mfamcoevol = matrix(NA, F, F)
 for (a in 1:F){
  for (b in 1:F){
   if (a==b){ m = NA 
+  }else{ if (a<b){ 
+   m = lfamcoevol[[fams[a]]][[fams[b]]]
   }else{
-   m = lfamcoevol[[fam[a]]][[fam[b]]]
-   if (is.null(m)){ m = lfamcoevol[[fam[b]]][[fam[a]]] }
-   mfamcoevol[a,b] = mfamcoevol[b,a] = m
-  }
+   m = lfamcoevol[[fams[b]]][[fams[a]]]
+  }}
+  mfamcoevol[a,b] = mfamcoevol[b,a] = m
  }
 }
+colnames(mfamcoevol) = rownames(mfamcoevol) = fams
+write.table(mfamcoevol, file=paste(outprefix, outtag, 'family_coevol_scores.mat', sep='.'), sep='\t')
+mtestfamcoevol = mfamcoevol[testfams, testfams]
+minscore = min(mfamcoevol)
+for (i in 1:ncol(mfamcoevol)){ mfamcoevol[i,i] = minscore } # to keep colour scaling
 
-names(mfamcoevol) = list(fams, fams)
+pdf(paste(outprefix, outtag, 'family_coevol_scores.pdf', sep='.'), height=30, width=30)
+heatmap(mfamcoevol, main='coevolution scores',
+		col=colorRampPalette(brewer.pal(8, "Greens"))(25), scale='none')
+dev.off()
 
-heatmap(mfamcoevol)
+write.table(mfamcoevol[testfams, testfams], file=paste(outprefix, outtag, 'test_family_coevol_scores.mat', sep='.'), sep='\t')
+matscorepval = sapply(testfams, function(fam1){
+  sapply(testfams, function(fam2){
+	s = mfamcoevol[fam1, fam2]
+	bg1 = mfamcoevol[fam1, controlfams]
+	bg2 = mfamcoevol[fam2, controlfams]
+	bg = sort(c(bg1, bg2), decreasing=T)
+	bg = bg[!is.na(bg)]
+	for (k in 1:length(bg)){ if (s > bg[k]){ break } }
+	return(k/length(bg))
+  })
+})
+rownames(matscorepval) = colnames(matscorepval) = testfams
+write.table(matscorepval, file=paste(outprefix, outtag, 'test_family_coevol_pvalues.mat', sep='.'), sep='\t')
+
+
+pdf(paste(outprefix, outtag, 'test_family_coevol_scores_pvalues.pdf', sep='.'), height=15, width=15)
+h = heatmap(mfamcoevol[testfams, testfams],
+	 main='coevolution scores (test gene families only)',
+	 col=colorRampPalette(brewer.pal(8, "Greens"))(25), scale='none')
+heatmap(matscorepval, Rowv=h$Rowv, Colv=h$Colv,
+	 main='coevolution p-values (test gene families only)',
+	 col=colorRampPalette(brewer.pal(8, "Reds"))(25), scale='none')
+dev.off()
+	
