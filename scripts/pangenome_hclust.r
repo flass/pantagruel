2e2764a35ac04126c0d03050813a7dd9ecb4084e
Florent Lassalle <fl4@sanger.ac.uk>
2020-05-14 10:43:51 +0100

    fixes to prevent bugs in dependency R package pvclust to make pipeline task 8 to crash

--- scripts/pangenome_hclust.r
+++ scripts/pangenome_hclust.r
@@ -5,7 +5,6 @@ library(ape)
 
 cargs = commandArgs(trailingOnly=T)
 
-
 orthomatrad = cargs[1]
 nboot = as.numeric(cargs[2])
 
@@ -19,17 +18,19 @@ plot(jacc.dist.ward.clust)
 dev.off()
 write.tree(as.phylo(jacc.dist.ward.clust), file=sprintf('%s_gene_abspres.clustering.nwk', orthomatrad))
 
-jacc.dist.ward.pvclust = pvclust(genocount, method.dist='binary', method.hclust='ward.D2', parallel=T, nboot=nboot)
-save(jacc.dist.ward.pvclust, file=sprintf('%s_gene_abspres.pvclustering.RData', orthomatrad))
-pdf(file=sprintf('%s_gene_abspres.pvclustering.pdf', orthomatrad), width=16, height=9)
-plot(jacc.dist.ward.pvclust)
-dev.off()
-jacc.dist.ward.pvclust.tree = as.phylo(jacc.dist.ward.pvclust$hclust)
-for (support in c('bp', 'au')){
-	jacc.dist.ward.pvclust.tree$node.label = as.character(format(jacc.dist.ward.pvclust$edges[,support], digits=2))
-	write.tree(jacc.dist.ward.pvclust.tree, file=sprintf('%s_gene_abspres.pvclustering.%s.nwk', orthomatrad, support))
+if (!is.null(dynGet('isTRUE', NULL))){
+	jacc.dist.ward.pvclust = pvclust(genocount, method.dist='binary', method.hclust='ward.D2', parallel=T, nboot=nboot)
+	save(jacc.dist.ward.pvclust, file=sprintf('%s_gene_abspres.pvclustering.RData', orthomatrad))
+	pdf(file=sprintf('%s_gene_abspres.pvclustering.pdf', orthomatrad), width=16, height=9)
+	plot(jacc.dist.ward.pvclust)
+	dev.off()
+	jacc.dist.ward.pvclust.tree = as.phylo(jacc.dist.ward.pvclust$hclust)
+	for (support in c('bp', 'au')){
+		jacc.dist.ward.pvclust.tree$node.label = as.character(format(jacc.dist.ward.pvclust$edges[,support], digits=2))
+		write.tree(jacc.dist.ward.pvclust.tree, file=sprintf('%s_gene_abspres.pvclustering.%s.nwk', orthomatrad, support))
+	}
+	jacc.dist.ward.pvclust.tree$node.label = paste(format(jacc.dist.ward.pvclust$edges[,'au'], digits=2),
+												   format(jacc.dist.ward.pvclust$edges[,'bp'], digits=2), sep='/')
+	write.tree(jacc.dist.ward.pvclust.tree, file=sprintf('%s_gene_abspres.pvclustering.au-bp.nwk', orthomatrad, support))
+	write.nexus(jacc.dist.ward.pvclust.tree, file=sprintf('%s_gene_abspres.pvclustering.au-bp.nex', orthomatrad, support))
 }
-jacc.dist.ward.pvclust.tree$node.label = paste(format(jacc.dist.ward.pvclust$edges[,'au'], digits=2),
-                                               format(jacc.dist.ward.pvclust$edges[,'bp'], digits=2), sep='/')
-write.tree(jacc.dist.ward.pvclust.tree, file=sprintf('%s_gene_abspres.pvclustering.au-bp.nwk', orthomatrad, support))
-write.nexus(jacc.dist.ward.pvclust.tree, file=sprintf('%s_gene_abspres.pvclustering.au-bp.nex', orthomatrad, support))
