2501b801daf3be67eb16a8ec7b5448a79bd0ecec
Florent Lassalle <florentlassalle@hotmail.fr>
2020-06-24 19:06:47 +0100

    bug fix

--- scripts/pipeline/pantagruel_pipeline_06_gene_trees.sh
+++ scripts/pipeline/pantagruel_pipeline_06_gene_trees.sh
@@ -186,7 +186,7 @@ else
       fam=$(echo ${bngt} | cut -d'.' -f2 )
       colaln=${colalinexuscodedir}/${collapsecond}/collapsed_alns/${fam}-collapsed.nex
       colmlgt=${colalinexuscodedir}/${collapsecond}/collapsed_ML_genetrees/${fam}-collapsed.nwk
-      if [[ ! -s ${colaln} ! && -s ${colmlgt} ]] ; then
+      if [[ ! -s ${colaln} && ! -s ${colmlgt} ]] ; then
         echo ${gt} >> ${mlgenetreelist}_resume
       fi
     done
