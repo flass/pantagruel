a6c03f2c3bea96627b8ed94d06ab486e3941aef0
Florent Lassalle <fl4@sanger.ac.uk>
2020-07-21 09:41:37 +0100

    add completion checks in task 03

--- scripts/pipeline/pantagruel_pipeline_03_create_sqlite_db.sh
+++ scripts/pipeline/pantagruel_pipeline_03_create_sqlite_db.sh
@@ -24,16 +24,22 @@ cd ${database}
 ### create and populate SQLite database
 
 ## Genome schema: initiate and populate
+step1="initiating and populating database (genome-related tables)" && echo ${step1}
 ${ptgscripts}/pantagruel_sqlitedb_genome.sh ${database} ${sqldbname} ${genomeinfo}/assembly_metadata ${genomeinfo}/assembly_info ${protali} ${protfamseqs}.tab ${protorfanclust} ${cdsorfanclust} ${straininfo} ${gblikeass} ${gp2ass}
+checkexec "something went wrong while ${step1}" "succefully completed ${step1}"
 
 # dump reference table for translation of genome assembly names into short identifier codes (uing UniProt "5-letter" code when available).
 sqlite3 ${sqldb} "select assembly_id, code from assemblies;" | sed -e 's/|/\t/g' > ${database}/genome_codes.tab
+checkexec "something went worng while generating 'genome_codes.tab' file"
 sqlite3 ${sqldb} "select code, organism, strain from assemblies;" | sed -e 's/|/\t/g' > ${database}/organism_codes.tab
+checkexec "something went worng while generating 'organism_codes.tab' file"
 # and for CDS names into code of type "SPECIES_CDSTAG" (ALE requires such a naming)
 # here split the lines with '|' and remove GenBank CDS prefix that is always 'lcl|'
 sqlite3 ${sqldb} "select genbank_cds_id, cds_code from coding_sequences;" | sed -e 's/lcl|//g'  | sed -e 's/|/\t/g' > ${database}/cds_codes.tab
+checkexec "something went worng while generating 'cds_codes.tab' file"
 
 # translates the header of the alignment files
+step2="translation of the header of the CDS alignment files from locus tags to CDS codes"
 for mol in prot cds ; do
   alifastacodedir=${protali}/full_${mol}fam_alignments_species_code
   mkdir -p ${alifastacodedir}
@@ -41,7 +47,10 @@ for mol in prot cds ; do
   # use multiprocessing python script
   ${ptgscripts}/lsfullpath.py "${protali}/full_${mol}fam_alignments/*.aln" > ${protali}/full_${mol}fam_alignment_list
   ${ptgscripts}/genbank2code_fastaseqnames.py ${protali}/full_${mol}fam_alignment_list ${database}/cds_codes.tab ${alifastacodedir} > ${ptgdb}/logs/genbank2code_fastaseqnames.${mol}.log
+  checkexec "something went wrong during ${step2} (${mol})" "succefully completed ${step2} (${mol})"
 done
 
 ## Phylogeny schema: initiate
+step3="initiating database (phylogeny-related tables)" && echo ${step1}
 sqlite3 ${sqldb} < ${ptgscripts}/pantagruel_sqlitedb_phylogeny_initiate.sql
+checkexec "something went wrong while ${step3}" "succefully completed ${step3}"
